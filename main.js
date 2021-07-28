e8c094b13f0ee161f245782c92cc0430783cc596
s.ischanova <s.ischanova@thecoders.ru>
2021-07-28 18:04:35 +0300

    диалоговые окна и условные операторы

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-let name = "Петя";
-let weight = 10;
-let boolean = true;
-console.log(name+" самый жирный как поезд пассажирный. Его вес - "+ weight + " тонн. Это информация 100% "+ boolean);
+if (confirm("Удалить ваш аккаунт?"))
+{
+    alert("Удалено.");
+}
+else{
+    alert("Вы молодец! Умничка!");
+}
