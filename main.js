f3060f4ff9923aad549b2ae808885b522ff23bf8
Sara894 <t48250klo@gmail.com>
2021-07-29 17:27:46 +0300

    подготовка к асинхронности

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
-//callback функции
-
-const foo = function(){
-    return "Hello, world!";
-};
-
-//вызываем функцию и выводим результат в консоль
-console.log(foo());
-
-//выводим саму функцию в консоль 
-console.log(foo);
-
-/* Значит, нам ничего не мешает передавать функцию как параметр
-других функций */
-
-const runIt = function(fn){
-    console.log("Вызов через функцию runIt!");
-    return fn();
+//синхронность
+function make_pizza(dough, cheeze, sausage)
+{
+    let pizza = "катаю лепешку из " + dough + ",кладу " + cheeze + ",тру на терке " + sausage + ",ставлю в духовку " ;
+    return pizza;
 }
+let dough = "1 кг теста";
+let cheeze = "1 кг сыра";
+let sausage = "1 кг колбасы";
+//готовим пиццу
+let pizza = make_pizza(dough, cheeze, sausage);
+console.log(pizza);
+//ждем пока ее приготовят - 2 часа это долго
+//едим пиццу
+function eat_pizza()
+{
+    return "пицца кончилась, всю съели";
+}
+pizza = eat_pizza();
+console.log(pizza)
 
-console.log(runIt(foo));
+//Хотим читать книгу пока готовят пиццу. Как это сделать?
+
+//попытка в асинхронность
+//3 функции - готовим, читаем, едим
+  
